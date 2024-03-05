3cc42017ffc0f33d8f0443012ca322809528bafa
Bruno <brunus.desenvolvimentobackfront@gmail.com>
2024-03-05 16:48:26 -0300

    Iniciado a criação de uma VPC e de um Cluster para processo de deploy na AWS

--- src/main/java/com/myorg/CdkAwsAluraInfraStack.java
+++ src/main/java/com/myorg/CdkAwsAluraInfraStack.java
@@ -3,22 +3,17 @@ package com.myorg;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecs.Cluster;
 
 public class CdkAwsAluraInfraStack extends Stack {
     public CdkAwsAluraInfraStack(final Construct scope, final String id) {
-        this(scope, id, null);
+        this(scope, id, null );
     }
 
     public CdkAwsAluraInfraStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "CdkAwsAluraInfraQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        
     }
 }
